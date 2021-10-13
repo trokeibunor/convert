036e1495a2c57eeac221d1bad05a112dd1988794
Emmanuel Okeibunor <okeibunoremma@gmail.com>
2021-10-13 09:27:17 +0100

    simple web page
    this converts a simple image to a simple page using tail css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          light: "#2d3953",
+          dark: "#2c3852",
+        },
+        "regal-blue": "#243c5a",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
